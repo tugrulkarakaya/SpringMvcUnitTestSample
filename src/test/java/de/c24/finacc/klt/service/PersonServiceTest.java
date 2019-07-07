b3b00816e930bb23fd6da1d452acc6793bbe8c1b
tugrul <tugrul@campaignup.co.uk>
2019-07-07 18:06:02 +0100

    Test Config is done

--- src/test/java/de/c24/finacc/klt/service/PersonServiceTest.java
+++ src/test/java/de/c24/finacc/klt/service/PersonServiceTest.java
@@ -6,7 +6,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -24,6 +27,18 @@ public class PersonServiceTest {
     @MockBean
     PersonRepository personRepository;
 
+    @Autowired
+    PersonService personService;
+
+    @TestConfiguration
+    static class BookServiceImplTestContextConfiguration {
+        @Bean
+        public PersonService personServiceService() {
+            return new PersonServiceImp();
+        }
+
+    }
+
     @Before
     public void setUp() {
         List<Person> personList = new ArrayList<>();
@@ -35,8 +50,9 @@ public class PersonServiceTest {
 
     @Test
     public void testAllRecords() {
-        Pageable pageable = PageRequest.of(0,  100, Sort.Direction.DESC, "publishedDate");
-        List<Person> personList = personRepository.findAllPeoplePagination(pageable);
+        List<Person> personList  = personService.getAllRecords();
+        //Pageable pageable = PageRequest.of(0,  100, Sort.Direction.DESC, "publishedDate");
+        //List<Person> personList = personRepository.findAllPeoplePagination(pageable);
         assertThat(personList.size()).isEqualTo(2);
     }
 
