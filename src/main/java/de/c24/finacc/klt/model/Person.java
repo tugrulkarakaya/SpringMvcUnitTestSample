1dd6af6249f13061e6d1d0f60c42426ca93a326e
tugrul <tugrul@campaignup.co.uk>
2019-07-07 17:58:47 +0100

    tiny bug fixProper Repository is implemented

---
+++ src/main/java/de/c24/finacc/klt/model/Person.java
@@ -0,0 +1,76 @@
+package de.c24.finacc.klt.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Integer age;
+
+    @NotBlank
+    @Size(max = 500)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 500)
+    private String lastName;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Person(Integer age, String firstName, String lastName) {
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Person(){
+
+    }
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", age=" + age +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
