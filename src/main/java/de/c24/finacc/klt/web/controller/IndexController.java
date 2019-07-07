71d872730bdf4e29c9019396a762305f184455b3
tugrul <tugrul@campaignup.co.uk>
2019-07-07 16:12:24 +0100

    Add files

--- src/main/java/de/c24/finacc/klt/web/controller/IndexController.java
+++ src/main/java/de/c24/finacc/klt/web/controller/IndexController.java
@@ -1,9 +1,17 @@
 package de.c24.finacc.klt.web.controller;
 
+import de.c24.finacc.klt.payload.Person;
+import de.c24.finacc.klt.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Index Controller
@@ -11,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 
+    @Autowired
+    PersonService personService;
     /**
      * the index page
      * @return ModelAndView for index page.
@@ -19,6 +29,25 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("formName", "Anmeldeformular");
 
+        return "index";
+    }
+
+    @PostMapping("/checkPerson")
+    public String saveCustomer(@ModelAttribute("person") Person person, Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl ="http://localhost:5050/klt/api/age";
+        ResponseEntity<String> response  = restTemplate.getForEntity(fooResourceUrl + "/26", String.class);
+        if(!response.getStatusCode().is2xxSuccessful())
+        {
+            model.addAttribute("error", "There is system error!");
+            return "index";
+        }
+        String greetingText = response.getBody();
+        if(greetingText == "OK" || greetingText =="Funny"){
+            personService.savePerson(person);
+            return "index";
+        }
+        model.addAttribute("error", "You can not register to the event!");
         return "index";
     }
 }
